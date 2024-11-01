5bf23f74b589a510465227c5f8c2f86db709426c
Diogo <dluganoxfarrapo@gmail.com>
2024-11-01 11:11:27 -0300

    vitrine

---
+++ vitrine248/vitrine.php
@@ -0,0 +1,30 @@
+<?php
+$email = $_POST["email"];
+$usuario = $_POST["usuario"];
+$senha = $_POST["senha"];
+
+
+// CONEXÃO COM BANCO DE DADOS
+// $conn = new MySQli('localhost','usuario','senha','banco de dados');
+$conn = new MySQli('localhost','root','','bd_vitrine');
+
+if($conn -> connect_error){
+    die("Connection failed:" . $conn -> connect_error);
+}else{
+    $sql = "INSERT INTO alunos (email, usuario, senha) VALUES ('$email', '$usuario', '$senha')";
+
+    if(mysqli_query($conn,$sql)){
+        if(mysqli_affected_rows($conn)){
+            session_start();//incia a sessão
+            $_SESSION["insert"] = "1";
+            header('Location:vitrine.html.php');
+            echo "Cadastro realizado com sucesso!";
+        }else{
+            $_SESSION["insert"] = "2";
+            echo "Falha ao cadastrar.";
+        }
+    }else{
+    echo "Falha no comando SQL.";
+    }
+}
+?>
